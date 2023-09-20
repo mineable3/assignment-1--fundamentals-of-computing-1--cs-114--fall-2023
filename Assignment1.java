a90f79570eb8522d655a54a0b49c95d44d51c544
Emmet Spaeth <emmet.spaeth@gmail.com>
2023-09-20 18:35:54 -0400

    \n added to test pushing to remote repo

--- Assignment1.java
+++ Assignment1.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
+    
     // Add your code here
     String inputString, reversedString = "";
     int randomNum;
